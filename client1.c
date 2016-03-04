a50486ae7b888549487c419e719e197db7231a7b
Konstantin Gorbunov <konstunn@ngs.ru>
2016-03-04 10:39:26 +0600

    Modifies client1 to use protobuf

--- client1.c
+++ client1.c
@@ -9,6 +9,7 @@
 #include <string.h> 
 #include <time.h> 
 #include "myqueue.h"
+#include "message.pb-c.h"
 
 int main()
 {
@@ -47,45 +48,54 @@ int main()
 		if (client_type == 1)
 		{
 			int tcp_sfd = socket(AF_INET, SOCK_STREAM, 0);
-			
-			// generate a message
-			unsigned char msg[MAX_MSG_LEN+1];
-			
-			int T;
-			do 
-				T = rand() % MAX_MSG_PROCTIME_SEC;
-			while (T == 0);
 
-			int len;
-			do 
-				len = rand() % MAX_MSG_LEN;
-			while (len == 0);
+			
+			// generate message
 
-			for (int i = 0; i < len; ++i) {
-				do {
-					msg[i] = rand() % UCHAR_MAX; 
-				} while (msg[i] <= 32 || msg[i] > 127);
+			Message message;
+			message__init(& message); 
+			
+			do 
+				message.t = rand() % MAX_MSG_PROCTIME_SEC;
+			while (message.t == 0);
+
+			do 
+				message.data.len = rand() % MAX_MSG_DATA_LEN;
+			while (message.data.len == 0);
+
+			uint8_t data[MAX_MSG_DATA_LEN+1];
+
+			for (int i = 0; i < message.data.len; ++i) {
+				do
+					data[i] = rand() % UCHAR_MAX; 
+				while (data[i] <= 32 || data[i] > 127);
 			}
+			message.data.data = data;
 
 			// send the message to server
+			
 			if (connect(tcp_sfd, (struct sockaddr*) & addr, sizeof(struct sockaddr_in)) < 0) {
 				warn("connect() failed");
 				continue;
 			}
 
+			int msg_size = message__get_packed_size(& message);
+
+			void *buf = malloc(msg_size);
+			message__pack(& message, (uint8_t*) buf);
+
 			write(tcp_sfd, & client_type, sizeof client_type);
-			write(tcp_sfd, & T, sizeof(int));
-			write(tcp_sfd, & len, sizeof(int));
-			write(tcp_sfd, & msg, len * sizeof(char));
+			write(tcp_sfd, buf, msg_size);
 
 			const int g_debug = 1;
 			if (g_debug) {
-				msg[len] = '\0';
-				warnx("sent \"%s\", len = %d, T = %d", msg, len, T);
+				message.data.data[message.data.len] = '\0';	
+				warnx("sent \"%s\", len = %lu, T = %d", message.data.data, message.data.len, message.t);
 			}
 
-			close(tcp_sfd);
+			free(buf);
 
+			close(tcp_sfd);
 		} 
 	}
 	
